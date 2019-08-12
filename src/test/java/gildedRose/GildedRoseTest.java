1b77890829b979f9244c1cb451f6d87ae387f38b
TTXXSS <1374230455@qq.com>
2019-08-12 20:39:35 +0800

    add:add test method should_return_quality_1_and_sellIn_10_when_given_item_with_quality_1_and_name_Sulfuras_Hand_of_Ragnaros_and_sellIn_10

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -14,5 +14,14 @@ public class GildedRoseTest {
         assertEquals(gildedRose.items[0].quality,0);
         assertEquals(gildedRose.items[0].sellIn,9);
     }
+    @Test
+    public void should_return_quality_1_and_sellIn_10_when_given_item_with_quality_1_and_name_Sulfuras_Hand_of_Ragnaros_and_sellIn_10(){
+        Item[] item = new Item[1];
+        item[0]= new Item("Sulfuras, Hand of Ragnaros",10,1);
+        GildedRose gildedRose = new GildedRose(item);
+        gildedRose.updateQuality();
+        assertEquals(gildedRose.items[0].quality,1);
+        assertEquals(gildedRose.items[0].sellIn,10);
+    }
 
 }
