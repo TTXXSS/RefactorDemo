fc7acab9a6104bc8ea466a31ae0d088d4555f4a2
TTXXSS <1374230455@qq.com>
2019-08-12 20:39:04 +0800

    add:add test method should_return_quality_0_and_sellIn_9_when_given_item_with_quality_1_and_name_Aged_and_sellIn_10

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -6,12 +6,13 @@ import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
     @Test
-    public void should_return_0_when_given_item_with_quality_1_and_name_Aged_and_sellIn_10(){
+    public void should_return_quality_0_and_sellIn_9_when_given_item_with_quality_1_and_name_Aged_and_sellIn_10(){
         Item[] item = new Item[1];
         item[0]= new Item("Aged",10,1);
         GildedRose gildedRose = new GildedRose(item);
         gildedRose.updateQuality();
         assertEquals(gildedRose.items[0].quality,0);
+        assertEquals(gildedRose.items[0].sellIn,9);
     }
 
 }
