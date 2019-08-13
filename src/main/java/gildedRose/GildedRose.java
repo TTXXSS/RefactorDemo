44834d6b5fbbb737557c124f8b90aac9faaca7dd
TTXXSS <1374230455@qq.com>
2019-08-13 20:35:40 +0800

    refactor: refactor method updateQuality in class OtherItem

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -11,49 +11,15 @@ public class GildedRose {
            switch (items[i].name) {
                case "Aged Brie":
                {
-                   if (items[i].quality < 50) {
-                       items[i].quality = items[i].quality + 1;
-                   }
-
-                   if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                       items[i].sellIn = items[i].sellIn - 1;
-                   }
-
-                   if (items[i].sellIn < 0) {
-                       if (items[i].quality < 50) {
-                           items[i].quality = items[i].quality + 1;
-                       }
-                   }
+                   new AgedItem().updateQuality(items[i]);
 
                }
                break;
 
                case "Backstage passes to a TAFKAL80ETC concert":
                 {
-                   if (items[i].quality < 50) {
-                       items[i].quality = items[i].quality + 1;
 
-                       if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                           if (items[i].sellIn < 11) {
-                               if (items[i].quality < 50) {
-                                   items[i].quality = items[i].quality + 1;
-                               }
-                           }
-
-                           if (items[i].sellIn < 6) {
-                               if (items[i].quality < 50) {
-                                   items[i].quality = items[i].quality + 1;
-                               }
-                           }
-                       }
-                   }
-
-                   if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                       items[i].sellIn = items[i].sellIn - 1;
-                   }
-                   if (items[i].sellIn < 0) {
-                       items[i].quality = items[i].quality - items[i].quality;
-                   }
+                  new BackstageItem().updateQuality(items[i]);
                }
                break;
 
@@ -64,18 +30,7 @@ public class GildedRose {
                break;
                default:
                {
-                    {
-                       items[i].quality = items[i].quality - 1;
-                   }
-
-                   items[i].sellIn = items[i].sellIn - 1;
-
-                   if (items[i].sellIn < 0) {
-                       if (items[i].quality > 0) {
-                           items[i].quality = items[i].quality - 1;
-
-                       }
-                   }
+                   new OtherItem().updateQuality(items[i]);
 
                }
                break;
