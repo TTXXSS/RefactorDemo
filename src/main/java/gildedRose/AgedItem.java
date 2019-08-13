457cf36bec1780ff124b1502d156b562c36e445f
TTXXSS <1374230455@qq.com>
2019-08-13 21:15:20 +0800

    refactor: add method updateQualityByQualityLess50() in class AgedItem

--- src/main/java/gildedRose/AgedItem.java
+++ src/main/java/gildedRose/AgedItem.java
@@ -3,18 +3,20 @@ package gildedRose;
 public class AgedItem implements Stratege {
     @Override
     public void updateQuality(Item item) {
-        if (isQualityLessThan50(item)) {
-            updateQualityAdd1(item);
-        }
+        updateQualityByQualityLess50(item);
 
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.sellIn = item.sellIn - 1;
         }
 
         if (item.sellIn < 0) {
-            if (isQualityLessThan50(item)) {
-                updateQualityAdd1(item);
-            }
+            updateQualityByQualityLess50(item);
+        }
+    }
+
+    private void updateQualityByQualityLess50(Item item) {
+        if (isQualityLessThan50(item)) {
+            updateQualityAdd1(item);
         }
     }
 
