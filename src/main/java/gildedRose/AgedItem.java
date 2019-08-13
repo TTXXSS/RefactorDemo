69dbe756cb502877ad0ef1cada662bfb9ed37158
TTXXSS <1374230455@qq.com>
2019-08-13 20:27:18 +0800

    refactor: refactor method updateQuality in class AgedItem

---
+++ src/main/java/gildedRose/AgedItem.java
@@ -0,0 +1,20 @@
+package gildedRose;
+
+public class AgedItem implements Stratege {
+    @Override
+    public void updateQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+}
